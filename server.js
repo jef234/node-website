b5bcac4b1b915e70fcf833baf4f8d9da5b01f0e8
Jeffrey Rajkumar <Jeffrey234@gmail.com>
2019-08-16 17:51:16 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,40 @@
+let express = require("express"),
+    PORT = 3000,
+    server = express(),
+    path = require("path");
+
+server.set("view engine", "ejs");
+
+server.get("/", function (req, res) {
+    // res.sendFile(path.join(__dirname,"home.html"));
+    // let data = { title: "Home", description: "This is my home page from express to EJS" }
+    res.render("index", { title: "Home", description: "This is my home page from express to EJS" });
+});
+
+server.get("/about-us", function (req, res) {
+    // res.send("<h1>About</h1>");
+    res.render("index", { 
+        title: "About Us", 
+        description: "This is about us page."
+     });
+});
+
+server.get("/services", function (req, res) {
+    // res.send("<h1>Services</h1>");
+    res.render("index", { 
+        title: "Services", 
+        description: "This is services we provide"
+     });
+});
+
+server.get("/contact-us", function (req, res) {
+    // res.send("<h1>Contact Us</h1>");
+    res.render("index", { 
+        title: "Contact Us", 
+        description: "This is how you can Contact Us."
+     });
+});
+
+server.listen(PORT, "localhost", function () {
+    console.log("Server started on port: " + PORT);
+});
