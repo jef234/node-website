a696cacf57bcb8d243a9e24ddc533cee2390ece2
Jeffrey Rajkumar <Jeffrey234@gmail.com>
2019-08-16 17:58:24 +0530

    Heroku Configuration

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 let express = require("express"),
-    PORT = 3000,
+    PORT = process.env.PORT || 3000,
     server = express(),
     path = require("path");
 
+server.use(express.static(path.join(__dirname, 'public')));
+
+server.set('views', path.join(__dirname, 'views'));
 server.set("view engine", "ejs");
 
 server.get("/", function (req, res) {
@@ -13,26 +16,26 @@ server.get("/", function (req, res) {
 
 server.get("/about-us", function (req, res) {
     // res.send("<h1>About</h1>");
-    res.render("index", { 
-        title: "About Us", 
+    res.render("index", {
+        title: "About Us",
         description: "This is about us page."
-     });
+    });
 });
 
 server.get("/services", function (req, res) {
     // res.send("<h1>Services</h1>");
-    res.render("index", { 
-        title: "Services", 
+    res.render("index", {
+        title: "Services",
         description: "This is services we provide"
-     });
+    });
 });
 
 server.get("/contact-us", function (req, res) {
     // res.send("<h1>Contact Us</h1>");
-    res.render("index", { 
-        title: "Contact Us", 
+    res.render("index", {
+        title: "Contact Us",
         description: "This is how you can Contact Us."
-     });
+    });
 });
 
 server.listen(PORT, "localhost", function () {
